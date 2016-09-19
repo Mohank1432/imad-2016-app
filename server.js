dc893152a1a88b7ddd6c3d7ec2def60f7b594c8c
Mohank1432 <mohank1432@gmail.com>
2016-09-19 14:55:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('article-1', function (req, res){
+   res.send("Hey guys these is my first article in my app"); 
+});
+
+app.get('article-2', function (req, res){
+   res.send("Hey guys these is my second article in my app"); 
+});
+
+app.get('article-3', function (req, res){
+   res.send("Hey guys these is my third article in my app"); 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
